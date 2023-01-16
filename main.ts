908e3333805919204fd344a8658cb35b0ac430c5
Jeremy Ng Cheng Hin <jngchenghin@microsoft.com>
2023-01-16 14:27:46 -0800

    First Observable and Observer

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-alert("ok USA or NOT!! Come to Japan!");
+import { from, Observer } from "rxjs";
+
+let numbers = [1, 5, 10];
+let source = from(numbers);
+
+class MyObserver implements Observer<number> {
+    next(value) {
+        console.log(`value: ${value}`);
+    }
+
+    error(e) {
+        console.log(`error ${e}`);
+    }
+
+    complete() {
+        console.log("complete");
+    }
+}
+
+source.subscribe(new MyObserver());
