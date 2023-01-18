aa4ab96ad8db2e0181fe9ca2e14d1e03959b37d1
Jeremy Ng Cheng Hin <jngchenghin@microsoft.com>
2023-01-18 10:52:50 -0800

    RxJS operators and optimizing bundle size

--- main.ts
+++ main.ts
@@ -1,20 +1,24 @@
-import { Observable } from "rxjs";
+// import { Observable, map, filter } from "rxjs";
+
+import { Observable } from "rxjs/internal/Observable";
+import { map } from "rxjs/internal/operators/map";
+import { filter } from "rxjs/internal/operators/filter";
 
 let numbers = [1, 5, 10];
-let source = new Observable(observer => {
+let source = new Observable<number>(observer => {
     let index = 0;
     let produceValue = () => {
         observer.next(numbers[index++]);
 
         if (index < numbers.length) {
-            setTimeout(produceValue, 2000);
+            setTimeout(produceValue, 200);
         } else {
             observer.complete();
         }
     }
 
     produceValue();
-});
+}).pipe(map(n => n * 2), filter(n => n > 4));
 
 source.subscribe({
     next: value => console.log(`value: ${value}`),
