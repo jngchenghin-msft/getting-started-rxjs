263693d2289674f763011997d2b1733206a2ab5e
Jeremy Ng Cheng Hin <jngchenghin@microsoft.com>
2023-01-19 12:31:10 -0800

    mergeMap to process inner observables

--- main.ts
+++ main.ts
@@ -1,30 +1,41 @@
+import { Observable } from "rxjs/internal/Observable";
 import { fromEvent } from "rxjs/internal/observable/fromEvent";
 import { map } from "rxjs/internal/operators/map";
 import { filter } from "rxjs/internal/operators/filter";
 import { delay } from "rxjs/internal/operators/delay";
+import { mergeMap } from "rxjs/internal/operators/mergeMap";
 
 let output = document.getElementById("output");
 let button = document.getElementById("button");
 let click = fromEvent(button, "click")
 
 const load = (url: string) => {
-    let xhr = new XMLHttpRequest();
+    return new Observable(observer => {
+        let xhr = new XMLHttpRequest();
 
-    xhr.addEventListener("load", () => {
-        let movies = JSON.parse(xhr.responseText);
-        movies.forEach(m => {
-            let div = document.createElement("div");
-            div.innerText = m.title;
-            output.appendChild(div);
-        });
+        xhr.addEventListener("load", () => {
+            let data = JSON.parse(xhr.responseText);
+            observer.next(data);
+            observer.complete();
+        })
+
+        xhr.open("GET", url);
+        xhr.send();
     })
-
-    xhr.open("GET", url);
-    xhr.send();
 }
 
-click.subscribe({
-    next: e => load("movies.json"),
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    })
+}
+
+// click.pipe(mergeMap(e => load("movies.json"))).subscribe(o => console.log(o));
+
+click.pipe(mergeMap(e => load("movies.json"))).subscribe({
+    next: renderMovies,
     error: err => console.log(`err: ${err}`),
     complete: () => console.log('complete')
 });
